10af4b113d5de09ae8a445461537e0ec468ca30d
staringstavs <122454826+staringstavs@users.noreply.github.com>
2023-01-11 20:00:16 +0700

    Add files via upload

---
+++ Data Diri.cpp
@@ -0,0 +1,32 @@
+//Judul
+    //BIODATA DIRI
+    //Oleh Masayoshi
+    //Selasa, 6 September 2022
+    //A11.2022.14005
+
+#include <iostream>
+using namespace std;
+
+//Kamus
+
+//Deskripsi
+
+main ()
+{
+    cout << "BIODATA DIRI" << endl;
+    cout << "Nama Lengkap         : Masayoshi Suryavanditya" << endl;
+    cout << "Nama Panggilan       : Oshi" << endl;
+    cout << "NIM                  : A11.2022.14005" << endl;
+    cout << "Tempat,Tanggal Lahir : Semarang, 17 Agustus 2004" << endl;
+    cout << "Jenis Kelamin        : Laki-Laki" << endl;
+    cout << "Golongan Darah       : O" << endl;
+    cout << "Alamat               : Jalan Keruing Raya No.101" << endl;
+    cout << "       RT/RW         : 007/017" << endl;
+    cout << "       Kel/Desa      : Srondol Wetan" << endl;
+    cout << "       Kecamatan     : Banyumanik" << endl;
+    cout << "Agama                : Islam" << endl;
+    cout << "Status Perkawinan    : Belum Kawin" << endl;
+    cout << "Pekerjaan            : Mahasiswa" << endl;
+    cout << "Kewarganegaraan      : Indonesia" << endl;
+    cout << "Sekian Terimakasih" << endl;
+}
